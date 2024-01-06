6ca3ca5b1410843c979b0b761b0304bbec1e0a87
bzenky <bzenky@gmail.com>
2024-01-06 09:13:39 -0300

    feat: cycles and start sound

--- components/timer.tsx
+++ components/timer.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import { Button } from "./ui/button";
 import { secondsToMinutes } from "date-fns";
+import useSound from 'use-sound';
+import { Button } from "./ui/button";
 
 export function Timer() {
   const [timer, setTimer] = useState(25 * 60)
+  const [cycle, setCycle] = useState(1)
   const [isActive, setIsActive] = useState(false)
+  const [playClockTick] = useSound("/assets/sounds/clock-tick.mp3")
 
   function handleTimer() {
     setIsActive(active => !active)
+
+    handleStart()
   }
 
   function resetTimer() {
@@ -22,7 +27,7 @@ export function Timer() {
 
     if (isActive) {
       interval = setInterval(() => {
-        setTimer((prevSeconds) => prevSeconds - 1);
+        setTimer((prevSeconds) => prevSeconds - 300);
       }, 1000);
     } else {
       clearInterval(interval);
@@ -35,6 +40,34 @@ export function Timer() {
   const secondsCalc = timer - (minutes * 60)
   const seconds = String(secondsCalc).padStart(2, '0')
 
+  const pomodoro = (60 * 25)
+  const shortBreak = (60 * 5)
+  const longBreak = (60 * 15)
+
+  function handleStart() {
+    if (isActive) return
+
+    playClockTick()
+  }
+
+  if (timer === 0) {
+    if (cycle % 2 === 0) {
+      if (cycle === 8) {
+        setCycle(1)
+        setTimer(pomodoro)
+      } else {
+        setCycle(cycle => cycle + 1)
+        setTimer(pomodoro)
+      }
+    } else if (cycle === 7) {
+      setTimer(longBreak)
+      setCycle(cycle => cycle + 1)
+    } else {
+      setCycle(cycle => cycle + 1)
+      setTimer(shortBreak)
+    }
+  }
+
   return (
     <div className="flex flex-col">
       <span className="text-center text-5xl font-semibold">
@@ -42,7 +75,10 @@ export function Timer() {
       </span>
 
       <div className="flex gap-4 mt-8">
-        <Button onClick={handleTimer} size="lg">
+        <Button
+          onClick={handleTimer}
+          size="lg"
+        >
           {
             isActive ? 'Pause' : 'Start'
           }
