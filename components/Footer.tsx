e9c1df2fea65de00bccbcf12014e110090ff584e
Zenky <bzenky@gmail.com>
2024-01-15 21:08:28 -0300

    style: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import { Code2 } from "lucide-react";
 
 export function Footer() {
   return (
-    <div className="flex flex-col justify-center items-center p-3 bg-zinc-400 w-full select-none gap-1">
+    <div className="flex flex-col justify-center items-center p-3 bg-red-800 w-full select-none gap-1 text-zinc-100">
       <span className=" flex items-center gap-2">
-        <Code2 className="w-4 h-4" /> by <a href="https://www.bzenky.dev" target="_blank" className="text-zinc-100 font-bold">Zenky</a>
+        <Code2 className="w-4 h-4" /> by <a href="https://www.bzenky.dev" target="_blank" className="font-bold">Zenky</a>
       </span>
 
       <span className="text-zinc-200">
