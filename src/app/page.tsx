153b7f908ad965aa0fb0366a66afb898d985aa3b
Zenky <bzenky@gmail.com>
2024-01-25 14:38:38 -0300

    style: home responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import { Controls } from "@/components/controls";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <div className="bg-red-400 p-16 w-full flex justify-center dark:bg-zinc-900 min-h-screen">
+      <div className="bg-red-400 p-8 xl:p-16 w-full flex justify-center dark:bg-zinc-900 min-h-screen">
         <div className="max-w-5xl flex flex-col">
           <div className="flex items-center justify-center mb-20">
-            <h1 className="text-2xl text-white font-bold xs:text-5xl select-none">Pomo-Pomodoro</h1>
+            <h1 className="text-3xl text-white font-bold xs:text-5xl select-none">Pomo-Pomodoro</h1>
           </div>
 
           <Timer />
